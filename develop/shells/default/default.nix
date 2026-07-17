e294c7add599036551840f6b138d3cb32890f6f3
sobte <i@sobte.me>
2026-07-18 03:15:33 +0800

    migrate: snowfall-lib → purr, pre-commit-hooks → git-hooks
    - Replace snowfall-lib with purr flake framework
    - Rename pre-commit-hooks input to git-hooks
    - Rename checks/pre-commit-check to checks/git-hooks

--- develop/shells/default/default.nix
+++ develop/shells/default/default.nix
@@ -1,20 +1,16 @@
 {
   inputs,
   pkgs,
-  mkShell,
   system,
   ...
 }:
 let
-  commitCheck = inputs.self.checks.${system}.pre-commit-check.shellHook;
+  commitCheck = inputs.self.checks.${system}.git-hooks.shellHook;
 in
-mkShell {
+pkgs.mkShell {
   packages =
     [
       (inputs.agenix.packages.${system}.default.override {
-        # On non-Windows, non-macOS systems, 
-        # you need to ensure that the pcscd service is installed and running. 
-        # `services.pcscd.enable = true;`
         plugins = [ pkgs.age-plugin-yubikey ];
       })
     ]
@@ -24,14 +20,12 @@ mkShell {
       nano
     ]);
 
-  # If you are using vscode, I will automatically set EDITOR for easy editing.
   shellHook = ''
     ${commitCheck}
 
     if command -v code > /dev/null 2>&1 && [[ -z $SSH_CONNECTION ]]; then
-      # If you want to open in a new window, add `--new-window`
       export EDITOR='code --wait'
     fi
   '';
-  buildInputs = inputs.self.checks.${system}.pre-commit-check.enabledPackages;
+  buildInputs = inputs.self.checks.${system}.git-hooks.enabledPackages;
 }
